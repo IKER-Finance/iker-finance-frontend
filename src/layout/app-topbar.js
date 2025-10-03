3ffc44ba9d35fef70f393d4b70547461f41dbf9a
Md Asif Iqbal Ahmed <asif.ahmed9414@gmail.com>
2025-10-03 09:31:48 +0200

    feat: UI layout improvements and simplifications

--- src/layout/app-topbar.js
+++ src/layout/app-topbar.js
@@ -1,10 +1,10 @@
 'use client';
 
 import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { Dropdown } from 'primereact/dropdown';
+import { Menu } from 'primereact/menu';
 import { LayoutContext } from './context/layout-context';
 import { selectUser } from '../redux/feature/auth-slice';
 import { authService } from '../services/api-service/auth-service';
@@ -13,41 +13,47 @@ import PAGE_ROUTES from '../constants/page-constants';
 const AppTopbar = forwardRef((props, ref) => {
   const menubuttonRef = useRef(null);
   const topbarmenuRef = useRef(null);
-  const topbarmenubuttonRef = useRef(null);
+  const profileMenuRef = useRef(null);
 
   const { onMenuToggle } = useContext(LayoutContext);
   const { push } = useRouter();
-  const dispatch = useDispatch();
   
   const user = useSelector(selectUser);
   const userName = user?.firstName ? `${user.firstName} ${user.lastName}` : 'User';
-
-  const options = [
-    { name: 'Profile', code: 'profile' }, 
-    { name: 'Settings', code: 'settings' },
-    { name: 'Sign Out', code: 'signout' }
-  ];
+  const userInitials = user?.firstName 
+    ? `${user.firstName.charAt(0)}${user.lastName?.charAt(0) || ''}`.toUpperCase()
+    : 'U';
 
   useImperativeHandle(ref, () => ({
     menubutton: menubuttonRef.current,
     topbarmenu: topbarmenuRef.current,
-    topbarmenubutton: topbarmenubuttonRef.current,
   }));
-
-  const handleOptionClick = (option) => {
-    if (option.code === 'profile') {
-      push(PAGE_ROUTES.profile);
-    } else if (option.code === 'settings') {
-      push(PAGE_ROUTES.settings);
-    } else if (option.code === 'signout') {
-      signOut();
-    }
-  };
 
   const signOut = () => {
     authService.logout();
     push(PAGE_ROUTES.login);
   };
+
+  const menuItems = [
+    {
+      label: 'Profile',
+      icon: 'pi pi-user',
+      command: () => push(PAGE_ROUTES.profile),
+    },
+    {
+      label: 'Settings',
+      icon: 'pi pi-cog',
+      command: () => push(PAGE_ROUTES.settings),
+    },
+    {
+      separator: true,
+    },
+    {
+      label: 'Sign Out',
+      icon: 'pi pi-sign-out',
+      command: signOut,
+    },
+  ];
 
   return (
     <div className="layout-topbar">
@@ -58,29 +64,30 @@ const AppTopbar = forwardRef((props, ref) => {
       <button
         ref={menubuttonRef}
         type="button"
-        className="p-link layout-menu-button layout-topbar-button"
+        className="layout-menu-button"
         onClick={onMenuToggle}
-        aria-label="Menu Toggle Button"
       >
         <i className="pi pi-bars" />
       </button>
 
       <div className="layout-topbar-right-icons">
-        <div ref={topbarmenuRef} className="layout-topbar-menu profile-button">
-          <i className="pi pi-user" />
-          <span>{userName}</span>
-        </div>
+        <button 
+          ref={topbarmenuRef} 
+          className="profile-button"
+          onClick={(e) => profileMenuRef.current?.toggle(e)}
+          type="button"
+        >
+          <div className="profile-avatar">{userInitials}</div>
+          <span className="profile-name">{userName}</span>
+          <i className="pi pi-chevron-down profile-chevron" />
+        </button>
       </div>
 
-      <Dropdown
-        value={null}
-        onChange={(e) => handleOptionClick(e.value)}
-        options={options}
-        optionLabel="name"
-        placeholder="Menu"
-        className="p-link layout-topbar-menu-button layout-topbar-button"
-        panelClassName="layout-topbar-dropdown"
-        ref={topbarmenubuttonRef}
+      <Menu 
+        model={menuItems} 
+        popup 
+        ref={profileMenuRef} 
+        className="profile-dropdown-menu"
       />
     </div>
   );
