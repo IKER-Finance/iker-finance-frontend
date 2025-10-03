e268804532b251d4718115c19b74c2af22f3ba80
Md Asif Iqbal Ahmed <asif.ahmed9414@gmail.com>
2025-10-03 09:31:48 +0200

    feat: UI layout improvements and simplifications

---
+++ src/app/dashboard/support/faq/page.js
@@ -0,0 +1,7 @@
+'use client';
+
+import UnderConstruction from '../../../../components/common/under-construction';
+
+export default function FAQPage() {
+  return <UnderConstruction pageName="Help & FAQ" />;
+}
